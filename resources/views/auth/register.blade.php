eb314a8f3c516b6fed894f4e60cb9020c2e01e38
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-04-01 23:48:26 -0500

    Agregando formulario arreglar errores en el formulario, como id, a for en los label y corregir el username y password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,3 +3,54 @@
 @section('titulo')
     Registrate en la pagina
 @endsection
+
+@section('contenido')
+    <div class="md:flex">
+        <div class="md:w-1/2">
+            <p>Imagen aqui</p>
+        </div>
+
+        <div class="md:w-1/2">
+            <form>
+                <div class="mb-5">
+                    <label id="name" class="mb-2 block uppercase text-gray-500 font bold">
+                        Nombre
+                    </label>
+                    <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu nombre"
+                        class="border p-3 w-full rounded-lg"
+                        >
+                </div>
+
+                <div class="mb-5">
+                    <label id="name" class="mb-2 block uppercase text-gray-500 font bold">
+                        USERNAME
+                    </label>
+                    <input 
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu nombre"
+                        class="border p-3 w-full rounded-lg"
+                        >
+                </div>
+     
+                <div class="mb-5">
+                    <label id="email" class="mb-2 block uppercase text-gray-500 font bold">
+                        Email
+                    </label>
+                    <input 
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu email"
+                        class="border p-3 w-full rounded-lg"
+                        >
+                </div>
+     
+            </form>
+        </div>
+    </div>
